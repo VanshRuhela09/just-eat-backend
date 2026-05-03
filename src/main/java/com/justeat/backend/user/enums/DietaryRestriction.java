bf86c5fffb8d909422e34d5c1729b89e496f11d9
vanshruhela <vansh.ruhela@nagarro.com>
2026-05-03 22:08:30 +0530

    fixes

--- src/main/java/com/justeat/backend/user/enums/DietaryRestriction.java
+++ src/main/java/com/justeat/backend/user/enums/DietaryRestriction.java
@@ -3,26 +3,9 @@ package com.justeat.backend.user.enums;
 public enum DietaryRestriction {
     VEGETARIAN,
     VEGAN,
+    HALAL,
     GLUTEN_FREE,
     DAIRY_FREE,
-    NUT_FREE,
-    HALAL,
-    KOSHER,
-    PESCATARIAN,
-    KETO,
-    LOW_CARB,
-    LOW_SODIUM,
-    LOW_FAT,
-    DIABETIC_FRIENDLY,
-    PALEO,
-    WHOLE30,
-    EGG_FREE,
-    SOY_FREE,
-    SHELLFISH_FREE,
-    LACTOSE_INTOLERANT,
-    ORGANIC,
-    RAW_FOOD,
-    FODMAP,
-    SUGAR_FREE
+    NUT_FREE
 }
 
